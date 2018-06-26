6172893c3dd27d2c7815e40d673fe6cfbee99f5b
Alex Bleakley <alex.bleakley@cloudera.com>
2018-06-26 15:43:18 -0400

    Fix indentation

--- bikeshare_deploy.py
+++ bikeshare_deploy.py
@@ -8,8 +8,7 @@ model = joblib.load('bikeshare_model.pkl')
 # Function returns predicted rides given features
 
 def predict_rides(args):
-
-    features = np.array([args['season'],args['yr'],args['mnth'],args['holiday'],args['weekday'],args['workingday'],args['weathersit'],args['temp'],args['atemp'],args['hum'],args['windspeed']]).reshape(1, -1)
+  features = np.array([args['season'],args['yr'],args['mnth'],args['holiday'],args['weekday'],args['workingday'],args['weathersit'],args['temp'],args['atemp'],args['hum'],args['windspeed']]).reshape(1, -1)
   return {'predicted_rides': int(model.predict(features)[0])}
 
 predict_rides({
