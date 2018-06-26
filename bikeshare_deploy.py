ba6a783e58e0866c041ca7f55a24cb0c5adaf5d2
Alex Bleakley <alex.bleakley@cloudera.com>
2018-06-26 15:22:03 -0400

    Reshape predict input array to avoid deprecated usage

--- bikeshare_deploy.py
+++ bikeshare_deploy.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.externals import joblib
 
 # Load model
@@ -7,7 +8,8 @@ model = joblib.load('bikeshare_model.pkl')
 # Function returns predicted rides given features
 
 def predict_rides(args):
-  features = [args['season'],args['yr'],args['mnth'],args['holiday'],args['weekday'],args['workingday'],args['weathersit'],args['temp'],args['atemp'],args['hum'],args['windspeed']]
+
+    features = np.array([args['season'],args['yr'],args['mnth'],args['holiday'],args['weekday'],args['workingday'],args['weathersit'],args['temp'],args['atemp'],args['hum'],args['windspeed']]).reshape(1, -1)
   return {'predicted_rides': int(model.predict(features)[0])}
 
 predict_rides({
